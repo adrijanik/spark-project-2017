5b0ee9a5d76349ef370c00df76ee5df29b7d675f
johnsmithm <moshnoi2000@gmail.com>
2017-10-23 11:52:27 +0200

    added reading

--- spamTopWords/src/main/scala/spamTopWord.scala
+++ spamTopWords/src/main/scala/spamTopWord.scala
@@ -10,10 +10,20 @@ object spamTopWord {
   def probaWordDir(sc:SparkContext)(filesDir:String)
   :(RDD[(String, Double)], Long) = {
 
-	val probaWord: RDD[(String, Double)] = sc.emptyRDD[(String, Double)]
-	val nbFiles = 0
+	val rdd = sc.wholeTextFiles(filesDir)
+        // The number of files is counted and stored in a variable nbFiles
+        val nbFiles = rdd.count()
+        // Non informative words must be removed from the set of unique words. We have also added ! and ?
+    	val stopWords = Set(".", ":", ",", " ", "/", "\\", "-", "'", "(", ")", "@")
+    	// Each text file must be splitted into a set of unique words (if a word occurs several times, it is saved only one time in the set).
+    	val wordBagRdd: RDD[(String, Set[String])] = rdd.map(textTuple => 
+		(textTuple._1, textTuple._2.trim().
+		split("\\s+").toSet.diff(stopWords)))
+    	// Get the Number of occurrences amongst all files
+    	val wordCountRdd: RDD[(String, Int)] = wordBagRdd.flatMap(x => x._2.map(y => (y, 1))).reduceByKey(_ + _)
+    	val probaWord: RDD[(String, Double)] = wordCountRdd.map(x => (x._1, x._2.toDouble / nbFiles))
+        return (probaWord, nbFiles)
 
-	return (probaWord, nbFiles)
   }
 
 
@@ -30,9 +40,18 @@ object spamTopWord {
   }
 
   def main(args: Array[String]) {
+  	if(args.size > 0)
+		val conf = new SparkConf().setAppName("Spam Filter Application").setMaster("local")
+		val sc = new SparkContext(conf)
+		println("Got the path:"+args(0))
+		// args(0) should be something like "hdfs:///project/
+		val ff = args(0)+"ham/*.txt"
+		val (probaW, nbFiles) = probaWordDir(sc)(ff)
+		println("number of files in "+ ff +":")
+		println(nbFiles)
+	else
+		println("Please write te directory where the ham and span")       
 
-  
-	println("it works!")
 
   }
 
