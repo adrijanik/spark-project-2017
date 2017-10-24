159125a1ff8811793597ae673235b3cf69180449
johnsmithm <moshnoi2000@gmail.com>
2017-10-24 14:05:53 +0200

    added solution

--- spamTopWords/src/main/scala/spamTopWord.scala
+++ spamTopWords/src/main/scala/spamTopWord.scala
@@ -36,15 +36,17 @@ object spamTopWord {
 
 
   def computeMutualInformationFactor(
-    probaWC:RDD[(String, Double)],
-    probaW:RDD[(String, Double)],
-    probaC: Double,
+    probaWC: RDD[(String, Double)],//prob of just a class, some word could not be 
+    probaW: RDD[(String, Double)],//all words prob, all word
+    probaC: Double, //prb of a class : class mails / all mails
     probaDefault: Double // default value when a probability is missing
-  ):RDD[(String, Double)] = {
-
-   
-	return probaWC
-
+  ): RDD[(String, Double)] = {
+	    //p(occurs) = 
+	    val probWJoin: RDD[(String, (Double, Option[Double]))] = probaW.leftOuterJoin(probaWC)// got all class probs, if not -> default
+				//p(accurs)  p(accurs,class) 
+	    val valueClassAndOcu: RDD[(String, (Double, Double))] = probWJoin.map(x => (x._1, (x._2._1, x._2._2.getOrElse(probaDefault))))
+	    //We have to change ln to log2 (by using ln(x)/ln(2)=log2(x)
+	    valueClassAndOcu.map(x => (x._1, x._2._2 * (math.log(x._2._2 / (x._2._1 * probaC)) / math.log(2.0))))
   }
 
   def main(args: Array[String]) {
@@ -57,13 +59,37 @@ object spamTopWord {
 
 		//process ham files
 		val (probaHW, nbHFiles) = probaWordDir(sc)(args(0)+"ham/*.txt")
-		print("number of files in "+ args(0)+"ham/*.txt" +":")
-		println(nbHFiles)
 
 		//process span files
 		val (probaSW, nbSFiles) = probaWordDir(sc)(args(0)+"span/*.txt")
+		print("number of files in "+ args(0)+"ham/*.txt" +":")
+		println(nbHFiles)
 		print("number of files in "+ args(0)+"span/*.txt" +":")
 		println(nbSFiles)
+
+		val nbFiles = nbSFiles + nbHFiles
+		val probaW = probaSW.union(probaHW).reduceByKey((x,y) => (x*nbSFiles.toDouble+y*nbSFiles.toDouble)/(nbFiles.toDouble)) //not sure
+
+	        //Compute the probability P(occurs, class) for each word.
+
+		val probaH = nbHFiles.toDouble / nbFiles.toDouble // the probability that an email belongs to the given class.
+		val probaS = nbSFiles.toDouble / nbFiles.toDouble
+		// Compute mutual information for each class and occurs
+		val MITrueHam = computeMutualInformationFactor(probaHW, probaW, probaH, 0.2 / nbFiles) // the last is a default value
+		val MITrueSpam = computeMutualInformationFactor(probaSW, probaW, probaS, 0.2 / nbFiles)
+		val MIFalseHam = computeMutualInformationFactor(probaHW.map(x => (x._1, 1 - x._2)), probaW, probaH, 0.2 / nbFiles)
+		val MIFalseSpam = computeMutualInformationFactor(probaSW.map(x => (x._1, 1 - x._2)), probaW, probaS, 0.2 / nbFiles)
+
+		//compute the mutual information of each word as a RDD with the map structure: word => MI(word)
+		//sum the prob for all words
+		val MI :RDD[(String, Double)] = MITrueHam.union(MITrueSpam).union(MIFalseHam).union(MIFalseSpam).reduceByKey( (x, y) => x + y)
+
+		// print on screen the 10 top words (maximizing the mutual information value)
+		//These words must be also stored on HDFS in the file “/tmp/topWords.txt”.
+	        val path: String = "/tmp/topWords.txt"
+		val topTenWords: Array[(String, Double)] = MI.top(10)(Ordering[Double].on(x => x._2))
+		//coalesce to put the results in a single file
+		sc.parallelize(topTenWords).keys.coalesce(1, true).saveAsTextFile(path)
 	}
 	else
 		println("Please write te directory where the ham and span")       
